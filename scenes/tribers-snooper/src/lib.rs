2266f05b4c816f0954f5aa9c9037e7b6fd22129f
cuprumwz <cuprumwz@foxmail.com>
2023-11-19 01:15:21 +0800

    serialport

--- scenes/tribers-snooper/src/lib.rs
+++ scenes/tribers-snooper/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(unused_imports, dead_code)]
+use serialport::{available_ports, SerialPortType};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn show_info() {
+    println!("test");
 }
