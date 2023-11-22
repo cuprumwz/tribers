0900b1ee3c27a01cc9e6a657486d2625104dc9e1
cuprumwz <cuprumwz@foxmail.com>
2023-11-22 22:34:29 +0800

    sigrok demo

--- scenes/tribers-snooper/src/main.rs
+++ scenes/tribers-snooper/src/main.rs
@@ -1,7 +1,13 @@
-//#![allow(unused_imports, dead_code)]
+// #![allow(unused_imports, dead_code)]
+use sigrok::Sigrok;
+use sigrok::Session;
+use sigrok::data::{Datafeed, Logic};
+use sigrok::config::{config_items, Configurable};
+use std::error::Error;
 use serialport::{available_ports, SerialPortType};
 
-fn main() {
+fn main()-> Result<(), Box<dyn Error>> {
+    println!("list serial port");
     match available_ports() {
         Ok(ports) => {
             match ports.len() {
@@ -31,4 +37,42 @@ fn main() {
             eprintln!("{:?}", e);
         }
     }
+
+    println!("list sigrok driver");
+    let ctx = Sigrok::new()?;
+    for driver in ctx.drivers() {
+        println!("---- {:?}: {} v{}",
+                 driver.name(),
+                 driver.long_name(),
+                 driver.api_version());
+    }
+
+    let ctx = Sigrok::new()?;
+    let ses = Session::new(&ctx)?;
+
+    let driver = ctx.drivers().into_iter().find(|x| x.name() == "demo").unwrap();
+    let driver = driver.init()?;
+    for device in driver.scan(None)? {
+        ses.add_device(&device)?;
+        device.config_set(config_items::LimitSamples, &64)?;
+
+        if let Some(group) = device.channel_groups().get(0) {
+            group.config_set(config_items::PatternMode, "sigrok")?;
+        }
+
+        device.config_set(config_items::SampleRate, &1_000_000)?;
+    }
+
+    ses.start(None, |_, data| match data {
+        Datafeed::Logic(Logic { unit_size, data }) => {
+            //let _ = unit_size;
+            println!("unit_size {}", unit_size);
+            for byte in data {
+                println!("{}", format!("{:08b}", byte).replace("1", "-").replace("0", "+"));
+            }
+        }
+        _ => {}
+    })?;
+
+    Ok(())
 }
