b26afc71bc85056dbc001da4c77d91bae1753daa
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-04-11 22:17:03 +0800

    struct

--- scenes/tribers-spawner/src/lib.rs
+++ scenes/tribers-spawner/src/lib.rs
@@ -1,25 +1,25 @@
-pub struct Menage {
-    name: String,
-}
-
-pub struct Master {
-
-}
-
-pub struct Spouse {
-
-}
-
-pub struct Snooper {
-
-}
-
-pub enum Calibre {
-    Voltage(voltage),
-}
-
-// 一个装备，本质上是一个设备，一个设备包含多个still
-pub struct Toolkit {
-    calibres: Vec<Calibre>,
-    dev: Dev,
-}
+// pub struct Menage {
+//     name: String,
+// }
+//
+// pub struct Master {
+//
+// }
+//
+// pub struct Spouse {
+//
+// }
+//
+// pub struct Snooper {
+//
+// }
+//
+// pub enum Calibre {
+//     Voltage(voltage),
+// }
+//
+// // 一个装备，本质上是一个设备，一个设备包含多个still
+// pub struct Toolkit {
+//     calibres: Vec<Calibre>,
+//     dev: Dev,
+// }
