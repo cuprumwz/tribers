7111458b562a8b662ac6f350c38e21e4d7c2d6d6
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-05-07 21:32:45 +0800

    whisper

---
+++ scenes/tribers-spawner/src/lib.rs
@@ -0,0 +1,35 @@
+use std::{fs::File, io::BufReader, path::{Path, PathBuf}};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct RoleMaster {
+    pub binary: PathBuf,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Menage {
+    pub name: String,
+    pub step: u8,
+    pub master: RoleMaster,
+}
+
+// 上电初始化一次的参数 'static
+#[derive(Deserialize, Debug)]
+pub struct Innate {
+    pub shrine: PathBuf,        //spider数据库保存路径  assets.db
+
+    pub fibase_valver: PathBuf,
+    pub sibase_valver: PathBuf,
+    pub fibase_snaper: PathBuf,
+    pub sibase_snaper: PathBuf,
+    pub sibase_larder: PathBuf,
+    pub fibase_larder: PathBuf,
+    pub sxmass_larder: PathBuf,
+    pub fxmass_larder: PathBuf,
+    pub sxplug_larder: PathBuf,
+    pub fxplug_larder: PathBuf,
+
+    pub surname: String,
+    pub version: String,
+    pub menages: Vec<Menage>,
+}
