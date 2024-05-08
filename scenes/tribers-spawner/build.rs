4bf339fd9cd4d99b57e928697049ec01594bd24e
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-05-09 00:09:24 +0800

    prost-wkt

--- scenes/tribers-spawner/build.rs
+++ scenes/tribers-spawner/build.rs
@@ -1,11 +1,50 @@
-use std::io::Result;
-
-fn main() -> Result<()> {
-    prost_build::compile_protos(&["./utils/whisper.proto"], &["utils/"])?;
-    Ok(())
-}
+// use std::io::Result;
 
 // fn main() -> Result<()> {
-//     prost_build::compile_protos(&["src/items.proto"], &["src/"])?;
+//     prost_build::compile_protos(&["./utils/whisper.proto"], &["utils/"])?;
 //     Ok(())
 // }
+
+use std::{env, path::PathBuf};
+use prost_wkt_build::*;
+
+fn main() {
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let descriptor_file = out.join("descriptors.bin");
+    let mut prost_build = prost_build::Config::new();
+    prost_build
+        .type_attribute(
+            ".",
+            "#[derive(serde::Serialize,serde::Deserialize)]"
+        )
+        .extern_path(
+            ".google.protobuf.Any",
+            "::prost_wkt_types::Any"
+        )
+        .extern_path(
+            ".google.protobuf.Timestamp",
+            "::prost_wkt_types::Timestamp"
+        )
+        .extern_path(
+            ".google.protobuf.Value",
+            "::prost_wkt_types::Value"
+        )
+        .file_descriptor_set_path(&descriptor_file)
+        .compile_protos(
+            &[
+                "utils/whisper.proto"
+            ],
+            &["utils/"],
+        )
+        .unwrap();
+
+    let descriptor_bytes =
+        std::fs::read(descriptor_file)
+        .unwrap();
+
+    let descriptor =
+        FileDescriptorSet::decode(&descriptor_bytes[..])
+        .unwrap();
+
+    prost_wkt_build::add_serde(out, descriptor);
+}
