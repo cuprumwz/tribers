6770817f7c2bfacaa9f38c2aab4ce901263cdfd0
cuprumwz <cuprumwz@foxmail.com>
2024-05-22 23:09:18 +0800

    proto

--- scenes/tribers-spawner/build.rs
+++ scenes/tribers-spawner/build.rs
@@ -1,38 +1,44 @@
-// use std::io::Result;
+// use std::{env, path::PathBuf};
+// use prost_wkt_build::*;
 
-// fn main() -> Result<()> {
-//     prost_build::compile_protos(&["./utils/whisper.proto"], &["utils/"])?;
-//     Ok(())
+// fn main() {
+//     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+//     let descriptor_file = out.join("descriptors.bin");
+//     let mut prost_build = prost_build::Config::new();
+//     prost_build
+//         .type_attribute(
+//             ".",
+//             "#[derive(serde::Serialize,serde::Deserialize)]"
+//         )
+//         .file_descriptor_set_path(&descriptor_file)
+//         .compile_protos(
+//             &[
+//                 "utils/whisper.proto"
+//             ],
+//             &["utils/"],
+//         )
+//         .unwrap();
+
+//     let descriptor_bytes =
+//         std::fs::read(descriptor_file)
+//         .unwrap();
+
+//     let descriptor =
+//         FileDescriptorSet::decode(&descriptor_bytes[..])
+//         .unwrap();
+
+//     prost_wkt_build::add_serde(out, descriptor);
 // }
 
-use std::{env, path::PathBuf};
-use prost_wkt_build::*;
-
-fn main() {
-    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let descriptor_file = out.join("descriptors.bin");
-    let mut prost_build = prost_build::Config::new();
-    prost_build
+fn main() -> std::io::Result()> {
+    tonic_build::configure()
         .type_attribute(
             ".",
             "#[derive(serde::Serialize,serde::Deserialize)]"
         )
-        .file_descriptor_set_path(&descriptor_file)
-        .compile_protos(
-            &[
-                "utils/whisper.proto"
-            ],
-            &["utils/"],
-        )
+        .compile(&["utils/whisper.proto"], &["utils"])
         .unwrap();
 
-    let descriptor_bytes =
-        std::fs::read(descriptor_file)
-        .unwrap();
-
-    let descriptor =
-        FileDescriptorSet::decode(&descriptor_bytes[..])
-        .unwrap();
-
-    prost_wkt_build::add_serde(out, descriptor);
+    Ok(())
 }
+
--- scenes/tribers-spawner/src/lib.rs
+++ scenes/tribers-spawner/src/lib.rs
@@ -1,23 +1,34 @@
 use std::{fs::File, io::BufReader, path::{Path, PathBuf}};
 use serde::Deserialize;
 
-use prost_wkt_types::*;
-
-pub mod whisper {
-	include!(concat!(env!("OUT_DIR"), "/whisper.rs"));
+pub mod greeter {
+	include!(concat!(env!("OUT_DIR"), "/greeter.rs"));
 }
 
-pub use whisper::*;
+pub use greeter::*;
 
 // 上电初始化一次的参数 'static
+// $(path)/${surname}/${menage}/fibase_innate.json
+// $(path)/${surname}/${menage}/fibase_shrine/
+// $(path)/${surname}/${menage}/sibase_shrine/
+
 #[derive(Deserialize, Debug)]
 pub struct Innate {
-    pub shrine: PathBuf,        // spider数据库保存路径  assets.db
+	// 先天特性文件存放的位置
+	pub fibase_innate: PathBuf,
+	pub sibase_innate: PathBuf,
 
+	// 公共信息存放的位置，如数据库文件assets.db
+    pub fibase_shrine: PathBuf,
+    pub sibase_shrine: PathBuf,
+
+	// 参数存放的位置
     pub fibase_valver: PathBuf,
     pub sibase_valver: PathBuf,
+
     pub fibase_snaper: PathBuf,
     pub sibase_snaper: PathBuf,
+
     pub sibase_larder: PathBuf,
     pub fibase_larder: PathBuf,
     pub sxmass_larder: PathBuf,
@@ -27,5 +38,5 @@ pub struct Innate {
 
     pub surname: String,
     pub version: String,
-    pub menages: Vec<Menage>,
+    pub menages: Vec<Menage>
 }
